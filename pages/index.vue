3b4487fd6f1157c3f04003cc29fe2dc21a9b8f1d
Tam <owutnr@gmail.com>
2022-05-18 15:27:06 +0700

    login logout

--- pages/index.vue
+++ pages/index.vue
@@ -8,12 +8,12 @@
         <h2 class="text-center font-bold text-3xl">Welcome back!</h2>
         <p class="text-center text-sm text-gray mb-12">sign in to your account to continue</p>
         <p class="font-bold text-black mb-4">Username</p>
-        <input class="border border-gray2 rounded-md px-3 py-2 w-full" v-model="username" placeholder="Type your email" />
+        <input class="border border-gray2 rounded-md px-3 py-2 w-full" v-model="email" placeholder="Type your email" />
         <div class="flex items-center justify-between ">
           <p class="font-bold text-black mb-4 mt-4">Password</p>
           <p class="text-black text-sm" >Forgot password?</p>
         </div>
-        <input class="border border-gray2 rounded-md px-3 py-2 w-full mb-10 " v-model="password" placeholder="Type your password" />
+        <input class="border border-gray2 rounded-md px-3 py-2 w-full mb-10 " v-model="password" type="password" placeholder="Type your password" />
         <button @click="login()" class="border bg-yellow text-white w-full rounded-md py-2" >Login</button>
       </div>
     </div>
@@ -29,21 +29,24 @@ export default {
     return {
       email: '',
       password: '',
+      role:'',
     }
   },
   methods: {
-    async login(){
-      const response=await axios.get('http://localhost:3333/login',{
-        email:this.email,
-        password:this.password,
+    async login() {
+      const response = await axios.post('http://127.0.0.1:3333/login', {
+        email: this.email,
+        password: this.password,
       })
-      if(response.data==='invalid email or password') {
-        alert('อีเมลหรือรหัสผ่านผิด กรุณาป้อนใหม่อีกครั้ง')
-      }
-      else{
-        localStorage.setItem('token',response.data.token)
-        alert('เข้าสู่ระบบสำเร็จ')
-        this.$router.push('/Member/Main')
+      console.log(response)
+      localStorage.setItem('token', response.data.token)
+
+      if(response.data.message==='You have logged in successfully.') {
+        if(response.data.query.role==='hr') {
+            this.$router.push('/hr/home')
+          } else {
+            this.$router.push('/admin/home')
+        }
       }
     }
   }
